1fc052157cac4b8fdd1b728f6893478f3b09edc1
Nasser <nasser.kessas@outlook.com>
2023-02-28 17:50:56 +1000

    Added weights and biases initialisation

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,9 @@
+import numpy as np
 
 class Neural_Network (object):
 
     def __init__ (self, neuron_structure):
         self.layers = len(neuron_structure)
         self.neuron_structure = neuron_structure
+        self.biases = [np.random.randn(b, 1) for b in neuron_structure[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(neuron_structure[:-1], neuron_structure[1:])]
