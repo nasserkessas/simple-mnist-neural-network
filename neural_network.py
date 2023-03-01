4f8c476d437692571b39d703ba54c4d30933d18f
Nasser <nasser.kessas@outlook.com>
2023-03-01 15:09:09 +1000

    Added update mini-batch function

--- neural_network.py
+++ neural_network.py
@@ -32,11 +32,32 @@ class Neural_Network (object):
 
             # Update mini batches #
             for this_batch in mini_batches:
-                self.update_mini_batch(this_batch)
+                self.update_mini_batch(this_batch, eta)
 
-    def update_mini_batch(self, this_batch):
-        # Do gradient descent and backprop here #
+    def update_mini_batch(self, this_batch, eta):
+
+        # Initialise ∇b and ∇w with 0's #
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # Loop though each training sample in this_batch #
+        for x, y in this_batch:
+
+            # Compute gradient of Cost function #
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            
+            # Update ∇b and ∇w sum # 
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        
+        # Update weights and biases using equation v'=v−η∇C #
+        self.weights = [this_weight-(eta/len(this_batch)*this_nabla_w) for this_weight, this_nabla_w in zip(self.weights, nabla_w)]
+        self.biases = [this_bias-(eta/len(this_batch)*this_nabla_b) for this_bias, this_nabla_b in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        # Calculate derivative of Cost function for a single element using backpropagation #
         pass
+    
 
     @staticmethod
     def sigmoid(z):
