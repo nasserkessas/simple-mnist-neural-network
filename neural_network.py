8d9f2cd33e7b9988e2509495587db052ea97c824
Nasser <nasser.kessas@outlook.com>
2023-03-01 10:15:00 +1000

    Added shell of stocastic gradient descent function

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 import numpy as np
 
 class Neural_Network (object):
@@ -12,6 +14,18 @@ class Neural_Network (object):
         for b, w in zip(self.biases, self.weights):
             a = self.sigmoid(np.dot(w,a), b)
         return a
+    
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
+
+        for epoch in range(epochs):
+            shuffle(training_data)
+            mini_batches = [training_data[x:x+mini_batch_size] for x in range(len(training_data)/mini_batch_size)]
+            for this_batch in mini_batches:
+                self.update_mini_batch(this_batch)
+
+    def update_mini_batch(self, this_batch):
+        pass
 
     @staticmethod
     def sigmoid(z):
