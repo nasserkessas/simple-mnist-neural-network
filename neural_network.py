bcfd8ef313b6e45c16c7a39f385f4267a10c3a56
Nasser <nasser.kessas@outlook.com>
2023-03-01 10:12:55 +1000

    Added activation calculation and sigmoid function

--- neural_network.py
+++ neural_network.py
@@ -7,3 +7,12 @@ class Neural_Network (object):
         self.neuron_structure = neuron_structure
         self.biases = [np.random.randn(b, 1) for b in neuron_structure[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(neuron_structure[:-1], neuron_structure[1:])]
+
+    def calc_network_activations(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w,a), b)
+        return a
+
+    @staticmethod
+    def sigmoid(z):
+        return 1/1+np.exp(-z)
