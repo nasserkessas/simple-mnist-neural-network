80ca53fc8abffeaf7920e9d94e7593dca10c3b5d
Nasser <nasser.kessas@outlook.com>
2023-03-02 11:15:40 +1000

    Added backpropagation algorithm

--- neural_network.py
+++ neural_network.py
@@ -15,9 +15,10 @@ class Neural_Network (object):
         # Calculate activations for the next layer based on weights and biases of previous #
         for b, w in zip(self.biases, self.weights):
             
-            # Calculate a' = σ(wa + b) #
-            a = self.sigmoid(np.dot(w,a) + b)
-        return a
+            # Calculate z = wa + b, a' = σ(z) #
+            z = np.dot(w, a) + b
+            a = self.sigmoid(z)
+        return a, z
     
 
     def SGD(self, training_data, epochs, mini_batch_size, eta):
@@ -55,10 +56,50 @@ class Neural_Network (object):
         self.biases = [this_bias-(eta/len(this_batch)*this_nabla_b) for this_bias, this_nabla_b in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
-        # Calculate derivative of Cost function for a single element using backpropagation #
-        pass
+
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        previous_activation, activation_vectors, z_vectors = self.feedForward(x)
+
+        ###########################################################
+        # Need to review this section (don't properly understand) #
+        delta = self.cost_prime(previous_activation, y) * self.sigmoid_prime(z_vectors[-1])
+
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, np.array(activation_vectors[-2]).transpose())
+
+        for l in range(2, self.num_layers):
+            z = z_vectors[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, np.array(activation_vectors[-l-1]).transpose())
+
+        ###########################################################
+        
+        return (nabla_b, nabla_w)
     
+    def feedForward(self, input):
+        activation_vectors = [input]
+        z_vectors = []
+
+        previous_activation = input
+        for layer in self.layers:
+            previous_activation, previous_z = self.calc_layer_activations(previous_activation)
+            activation_vectors.append(previous_activation)
+            z_vectors.append(previous_z)
+        
+        return previous_activation, activation_vectors, z_vectors
+
 
     @staticmethod
     def sigmoid(z):
         return 1 / 1 + np.exp(-z)
+
+    @staticmethod
+    def cost_prime(a, y):
+        return a-y
+    
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z)*(1-self.sigmoid(z))
